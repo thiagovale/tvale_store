e4e97aba581d4c3054b12874db16e07cf3177c95
Thiago Vale <thiagopvale@hotmail.com>
2023-03-29 08:57:55 -0300

    fixing category model

--- models/category.js
+++ models/category.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Category = new Schema({
+const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
 });
 
 // Virtual for category's URL
-Category.virtual("url").get(function () {
+CategorySchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/category/${this._id}`;
 });
 
 // Export model
-module.exports = mongoose.model("Category", Category);
+module.exports = mongoose.model("Category", CategorySchema);
